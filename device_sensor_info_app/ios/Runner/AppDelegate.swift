93389215d3a8730c7ab8c7003ab85ca0876bf1fb
Jathinesh Kottem <jathinesh.kottem@gmail.com>
2025-09-21 17:26:14 +0530

    add ios implementation for battery level

--- device_sensor_info_app/ios/Runner/AppDelegate.swift
+++ device_sensor_info_app/ios/Runner/AppDelegate.swift
@@ -5,9 +5,35 @@ import UIKit
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let batteryChannel = FlutterMethodChannel(name: "device.info/battery",
+                                              binaryMessenger: controller.binaryMessenger)
+    batteryChannel.setMethodCallHandler({
+      [weak self] call, result in
+      guard let self = self else { return }
+      switch call.method {
+      case "getBatteryLevel":
+        self.receiveBatteryLevel(result: result)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  private func receiveBatteryLevel(result: FlutterResult) {
+    let device = UIDevice.current
+    device.isBatteryMonitoringEnabled = true
+    if device.batteryState == UIDevice.BatteryState.unknown {
+      result(FlutterError(code: "UNAVAILABLE",
+                          message: "Battery level not available.",
+                          details: nil))
+    } else {
+      result(Int(device.batteryLevel * 100))
+    }
+}
 }
